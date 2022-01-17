af4decdd373e7362aa6af34de003b08ca0f7ec16
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-01-17 19:54:39 +0100

    17.01.2022

--- js/jsExamplesMenu.js
+++ js/jsExamplesMenu.js
@@ -1,24 +1,30 @@
-const someMain = document.querySelector(`.someMain`);
-const someMain1 = document.querySelector(`.someMain1`);
 
 
 
-const nowa = () =>
-{
-    setTimeout(() =>
-    {
-        someMain1.classList.add(`listOn`);
-    }, 300);
-}
-someMain.addEventListener(`mouseenter`, nowa);
-someMain1.addEventListener(`mouseover`, () =>
-{
-    clearTimeout(nowa);
-})
-someMain1.addEventListener(`mouseout`, function () 
-{
-    this.classList.remove(`listOn`);
-})
+// const sameMain = document.querySelector(`.sameMain`);
+// const sameMain1 = document.querySelector(`.sameMain1`);
+
+
+
+// const nowa = () =>
+// {
+//     setTimeout(() =>
+//     {
+//         sameMain1.classList.add(`listOn`);
+//     }, 300);
+// }
+// sameMain.addEventListener(`mouseenter`, nowa);
+// sameMain1.addEventListener(`mouseover`, () =>
+// {
+//     clearTimeout(nowa);
+// })
+// sameMain1.addEventListener(`mouseout`, function ()
+// {
+//     setTimeout(() =>
+//     {
+//         this.classList.remove(`listOn`);
+//     }, 500);
+// })
 
 
 
