d064522b6cd926245d637e5240e40ea512a5dc89
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-01-19 15:34:36 +0100

    19.01.2022
    zmiana kodu w pliku js obsługującym zdarzenie drugiego klawisza

--- js/jsExamplesMenu.js
+++ js/jsExamplesMenu.js
@@ -1,68 +1,7 @@
-const idList = document.querySelector(`ul.anyList`);
+const idList = document.getElementById(`artContact2`);
 
-function clickFunction()
+function showList()
 {
         document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`)
-};
-idList.addEventListener(`click`, clickFunction);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const sameMain = document.querySelector(`.sameMain`);
-// const sameMain1 = document.querySelector(`.sameMain1`);
-
-
-
-// const nowa = () =>
-// {
-//     setTimeout(() =>
-//     {
-//         sameMain1.classList.add(`listOn`);
-//     }, 300);
-// }
-// sameMain.addEventListener(`mouseenter`, nowa);
-// sameMain1.addEventListener(`mouseover`, () =>
-// {
-//     clearTimeout(nowa);
-// })
-// sameMain1.addEventListener(`mouseout`, function ()
-// {
-//     setTimeout(() =>
-//     {
-//         this.classList.remove(`listOn`);
-//     }, 500);
-// })
-
-
-
+}
+idList.addEventListener(`click`, showList);
