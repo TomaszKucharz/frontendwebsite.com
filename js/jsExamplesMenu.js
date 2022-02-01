0b2744fe5b6e3340d9d037de6a4b5b50c436f820
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-02-01 01:51:08 +0100

    01.02.2022

--- js/jsExamplesMenu.js
+++ js/jsExamplesMenu.js
@@ -4,5 +4,6 @@ function showList()
 {
    document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`)
    document.querySelector(`#anyList.anyList2`).classList.toggle(`listOn`)
+   document.querySelector(`#anyList.anyList3`).classList.toggle(`listOn`)
 }
 idList.addEventListener(`click`, showList);
