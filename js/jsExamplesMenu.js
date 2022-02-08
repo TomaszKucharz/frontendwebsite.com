deef14d46ffe2481e3cc4822f7f65791364b40e3
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-02-08 17:51:24 +0100

    8.02.2022
    next example to list

--- js/jsExamplesMenu.js
+++ js/jsExamplesMenu.js
@@ -1,9 +1,9 @@
 const idList = document.getElementById(`artContact2`);
 
-function showList()
-{
-   document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`)
-   document.querySelector(`#anyList.anyList2`).classList.toggle(`listOn`)
-   document.querySelector(`#anyList.anyList3`).classList.toggle(`listOn`)
+function showList() {
+	document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`);
+	document.querySelector(`#anyList.anyList2`).classList.toggle(`listOn`);
+	document.querySelector(`#anyList.anyList3`).classList.toggle(`listOn`);
+	document.querySelector(`#anyList.anyList4`).classList.toggle(`listOn`);
 }
 idList.addEventListener(`click`, showList);
