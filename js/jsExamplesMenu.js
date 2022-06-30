61fc1ca6533ca31fbf2410eb01e4eb0fb3c1e5b9
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-06-30 08:47:52 +0200

    add two links to js & html (cube & banner3D)

--- js/jsExamplesMenu.js
+++ js/jsExamplesMenu.js
@@ -1,9 +1,11 @@
 const idList = document.getElementById(`artContact2`);
 
 function showList() {
-	document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`);
-	document.querySelector(`#anyList.anyList2`).classList.toggle(`listOn`);
-	document.querySelector(`#anyList.anyList3`).classList.toggle(`listOn`);
-	document.querySelector(`#anyList.anyList4`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList1`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList2`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList3`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList4`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList5`).classList.toggle(`listOn`);
+  document.querySelector(`#anyList.anyList6`).classList.toggle(`listOn`);
 }
 idList.addEventListener(`click`, showList);
